23f128cf7bb2fbf3d5e3a44254a8e38dcb514743
atilayyuri <tamkan@th-brandenburg.de>
2023-05-22 21:32:21 +0200

    refactoring the view to class based views

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-#from django.views.decorators.csrf import  csrf_exempt
+# from django.views.decorators.csrf import  csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.views import APIView
+from django.http import Http404
 
 # https://www.django-rest-framework.org/api-guide/renderers/
 
 from posts.models import Post
 from posts.serializers import PostSerializer
+
 
 # when the posts requested from the API we need to get
 # that back and we can also request or update individual post
@@ -22,43 +24,50 @@ from posts.serializers import PostSerializer
 #     DELETE — Deletes the record at the given URI
 #     PATCH — Update individual fields of a record
 
-@api_view(['GET', 'POST'])
-def post_list(request, format=None):
+class PostList(APIView):
     # if there is get request, all posts needs to be get, serialized
     # and needs to be returned as JSON.
 
     # if there is post request, JSON data needs to be parsed,
     # serialized and if the data is valid it needs to be saved
     # and JSON response as success needs to be returned.
-    if request.method == 'GET':
+
+    def get(self, request, format=None):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
 
-    if request.method == 'POST':
+    def post(self, request, format=None):
         serializer = PostSerializer(data=request)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# details for the individual posts
-@api_view(['GET', 'PUT', 'DELETE'])
-def post_detail(request, pk, format=None):
-    try:
-        post = Post.objects.get(pk=pk)
-    except Post.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+class PostDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
         serializer = PostSerializer(post)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
         serializer = PostSerializer(post, data=request)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
